38f0565a9fe2985481fdf414ef4bb6b31da6c270
Tamara Bazko <tamarabazko@gmail.com>
2018-06-26 03:03:39 +0200

    Adding few environment conditions

--- src/services/knex.js
+++ src/services/knex.js
@@ -3,7 +3,9 @@ import path from 'path';
 
 let databaseUrl = 'postgresql://tam:md5f93eba6eb6b4db207f8e92d10e4ff066@127.0.0.1:5432/tvmaze';
 
-// databaseUrl += '?ssl=true';
+if (process.env.NODE_ENV === 'production') {
+  databaseUrl = 'postgres://bjbkiikufkhnen:f114a7d7178fe607d8a9120a1658bac85de7b5e90df2d2d25dac5088875c5afb@ec2-54-247-87-201.eu-west-1.compute.amazonaws.com:5432/d7amp7lu34alhn?ssl=true';
+}
 
 const knexConfig = {
   client: 'pg',
